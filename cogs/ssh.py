728c22f0fd4aa5a2c0abb0d66d6a1cf8f26c7130
goue0015 <pierre.gouedar@etudiant.univ-reims.fr>
2023-02-02 10:26:25 +0100

    update ssh cogs

--- cogs/ssh.py
+++ cogs/ssh.py
@@ -55,43 +55,5 @@ class ssh(commands.Cog):
         self.client.connect(self.host, username=self.username, password=self.password, port=self.port)
         _stdin, _stdout, _stderr = self.client.exec_command(f"docker restart {container}")
         await interaction.response.send_message("Restart request send to the container")
-
-    @app_commands.command(name="mccommand")
-    async def McCommand(self, interaction :discord.Interaction, command:str):
-        self.getDockerListFile()
-        testvar=False
-        with open('output.txt', 'r') as f:
-            for line in f.readlines():
-                if "tccr.io/truecharts/minecraft-java" in line:
-                    testvar=True
-                    self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                    self.client.connect(self.host, username=self.username, password=self.password, port=self.port)
-                    _stdin, _stdout, _stderr = self.client.exec_command(f"docker exec {line[0:12]}  mc-send-to-console {command}")
-                    f.close()
-                    os.remove("output.txt")
-                    if len(_stdout.read().decode())>0:
-                        await interaction.response.send_message(f"Logs : `{_stdout.read().decode()}`")
-                    else :
-                        await interaction.response.send_message(f"Nothing were printed in the console but the command was send succesfully.")
-        if testvar==False:
-            await interaction.response.send_message(f"No TrueCharts minecraft container were found. Maybe check the docker container list.")
-
-    @app_commands.command(name="getmcconsolelogs")
-    async def getMcConsoleLogs(self, interaction=discord.Interaction):
-        self.getDockerListFile()
-        with open('output.txt', 'r') as f:
-            for line in f.readlines():
-                if "tccr.io/truecharts/minecraft-java" in line:
-                    self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                    self.client.connect(self.host, username=self.username, password=self.password, port=self.port)
-                    _stdin, _stdout, _stderr = self.client.exec_command(f"docker logs {line[0:12]}")
-                    OFile = open("outputLogs.txt", "a")
-                    OFile.write(_stdout.read().decode())
-                    OFile.close()
-                    await interaction.response.send_message(file=discord.File("outputLogs.txt"))
-                    os.remove("outputLogs.txt")
-                    self.client.close()
-        os.remove("output.txt")
-
 async def setup(bot):
     await bot.add_cog(ssh(bot), guilds=[discord.Object(id=1064226985116704829)])
